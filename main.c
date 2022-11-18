e9e8a9a6d031867ff769b5c798a64be93bcf8ae8
Juan Montilla <75455653+Monti3@users.noreply.github.com>
2022-11-18 18:01:21 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+// declaro los prototipos de las funciones 
+int buscaprimos(int p);                              
+int sacar_cantidad_de_divisores(int p);
+// funcion principal: 
+int main()
+{
+int x;
+printf("Ingrese un numero: ");
+scanf("%d", &x);                        //pido al usuario que ingrese un numero 
+buscaprimos(x);                         // ingreso ese nunmero a la funcion de buscaprimos
+}
+
+// declaro la funcion bucaprimos
+int buscaprimos(int p)
+{
+  int cdivisores = sacar_cantidad_de_divisores(p); // declaro esta variable como la cantidad de divisores de el numero
+printf("%i es la cantidad de divisores", cdivisores); // muestro la cantidad de divisores del numero 
+
+  if (cdivisores == 2)
+  {
+    printf("\n%i es primo", p);          //a un numero se lo llama primo si la cantidad de divisores que tiene es exactamente 2
+  }
+  else
+  {
+    printf("\n%i no es primo", p);
+  } 
+  
+}
+
+int sacar_cantidad_de_divisores(int p)
+{
+  int cdivisores = 0;
+  {                                    
+  for (int x =1; x<=p; x++)            // busco los denominadores los cuales tengan resto 0 con numero indicado como numerador en la division 
